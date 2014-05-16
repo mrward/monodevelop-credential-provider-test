2211ca89e579bf34bad46c437d99e9098829cf99
Matt Ward <ward.matt@gmail.com>
2014-05-16 14:03:28 +0100

    Initial revision.

---
+++ CredentialProviderTest/CredentialProviderHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using System.Net;
+using System.Threading;
+
+namespace CredentialProviderTest
+{
+	public class CredentialProviderHandler : CommandHandler
+	{
+		public CredentialProviderHandler ()
+		{
+		}
+
+		protected override void Run (object dataItem)
+		{
+
+			for (int i = 0; i < 5; ++i) {
+				var thread = new Thread (() => {
+					PromptForCredentials ();
+				});
+
+				thread.Start ();
+			}
+		}
+
+		void PromptForCredentials ()
+		{
+			var cp = MonoDevelop.Core.WebRequestHelper.CredentialProvider;
+
+			var uri = new Uri ("http://nuget.org");
+			var proxy = new WebProxy ();
+
+			cp.GetCredentials (uri, proxy, MonoDevelop.Core.Web.CredentialType.ProxyCredentials, false);
+		}
+	}
+}
+
---
+++ package.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Xamarin Studio\bin\mdtool.exe" setup pack CredentialProviderTest\bin\Debug\CredentialProviderTest.dll
